ead71cf6bf0d76e31b115e6022d661ed2871171c
Daniellmass <daniel.mass@post.runi.ac.il>
2024-01-17 21:56:16 +0200

    checheckingTheTests

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,76 @@
 public class ArrayOps {
     public static void main(String[] args) {
+        int [] array = {1, 2, 1, 1, 2};
+        int [] array3 = {1, 3, 2};
+        System.out.println(isSorted(array3));
+       
         
-    }
-    
+        
+        
+    }  
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int a1 = 0;
+        int sumOfArr = 0;
+        // Sum of the elements if the array contains the missing int
+        int complSum = ((array.length + 1) * ( (2 * a1) + (array.length + 1 - 1) * 1)) / 2;
+        for(int i = 0; i < array.length; i++) {
+            sumOfArr += array[i];
+        }
+        int missNum = complSum - sumOfArr;
+         return missNum;   
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
-    }
+        int temp = 0;
+        //Sorting the array
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] > array [i]) {
+                    temp = array[i];
+                    array[i] = array [j];
+                    array[j] = temp;      
+                }
+            }
+        }
+        int secMax = array[array.length - 2];
+        return secMax;
+     }
+            
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
+       int counter = 0;
+       for(int i = 0; i < array1.length; i++) {
+        for(int j = 0; j < array2.length; j++) {
+            if (array1[i] == array2[j]) {
+                counter++;
+            }
+        }
+       }
+       if (counter == array2.length || counter == array1.length) {
+        return true;
+       } 
+       else {
         return false;
+       }
     }
+     
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
-    }
+        for(int i = 0; i < array.length - 2; i++) {
+            if (array[i] <= array[i+1]) {
+                if(array [i+1] > array[i + 2] ) {
+                    return false;
+                }
+            }
+            else {
+                    if (array [i+1] < array [i+2]) {
+                        return false;
+                    }
+                }
+                
+            }    
+        return true;
+     }
+    
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,73 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
+        String test = "Hello     world";
+        char char2 = 'l';
+        System.out.println(camelCase(test)); 
         
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String capVowelRest = "";
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U')  {
+                if ( ch > 'A' && ch <= 'Z') {
+                    capVowelRest += (char) (string.charAt(i) + 32); 
+                }
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'e' || ch == 'u') {
+                    capVowelRest += (char) (ch - 32);
+                }      
+            }
+             capVowelRest += (char) (ch);    
+        }
+        return capVowelRest;     
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String ans = "";
+        int temp = 0;
+        for( int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if((char) ch < 91 && (char) ch != 32) {
+                ans += (char) (ch + 32);
+            }
+            
+            else if (ch == 32 && string.charAt(i + 1) > 90){
+                ch = string.charAt(i+1);
+                ans += (char) (ch - 32);
+                i++;
+            }
+            else if (ch == 32 && string.charAt(i + 1) <= 90) {
+                temp = string.charAt(i + 1);
+                ans += (char) temp;
+                i++;
+            } 
+            else if ((char) ch != 32 ) {
+                ans += ch;
+            }
+            else if (ch == ' ') {
+            } 
+        
+    }
+        return ans;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
-    }
+        int counter = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if ( string.charAt(i) == chr) {
+                counter++;
+            }
+        }
+        int [] newArr = new int [counter];
+        int index = 0;
+        for(int j = 0; j < string.length(); j++) {
+            if (string.charAt(j) == chr) {
+            newArr[index] = j;
+            index++;
+            }
+        }
+        return newArr;
+        }      
 }
